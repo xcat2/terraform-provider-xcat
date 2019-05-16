ba3c6017249a02c1384bbbdd3149c05dba11abcb
xuweibj <xwbj@cn.ibm.com>
2019-05-16 04:16:57 -0400

    update code 0516

--- xcat/client_xcat.go
+++ xcat/client_xcat.go
@@ -2,41 +2,101 @@ package xcat
 
 import (
 	"net/http"
+	"time"
+	"fmt"
+	"strconv"
 )
 
-func Login(baseUrl string, username string, password, string) (string, error) {
-	url := baseUrl
+func FormatResponse(resp interface{}, err error) (interface{}, int, string) {
+	if err != nil {
+		errorcode_str := fmt.Sprintf("%s", err)
+		errorcode, _ := strconv.Atoi(errorcode_str)
+		errormsg := resp.(string)
+		return "", errorcode, errormsg
+	} else {
+		return resp, 0, ""
+	}
+}
+
+func Login(baseUrl string, username string, password string) (string, int, string) {
+	url := baseUrl + "/auth/login"
 	httpClient := http.Client{Timeout: time.Second * 30}
         client := &HttpClient{Client: &httpClient, Headers: http.Header{}}
 	data := make(map[string]interface{})
         data["username"] = username
 	data["password"] = password
-	ret, err := client.Post(url, nil, data)
-	if err != nil {
-		return "", err + " " + ret
+	ret, errcode, errmsg := FormatResponse(client.Post(url, nil, nil, data, false))
+	if errcode != 0 {
+		return "", errcode, errmsg
+	}	
+	token, isok := ret.(map[string]interface {})["token"].(map[string]interface {})["id"].(string)
+	if !isok {
+		return "", 1, "Failed to get token from response"
 	}
-	return ret
+	return token, errcode, errmsg
 }
 
-func ListNodeAttr(node string, baseUrl string, token string, attr string) (string, error) {
+func ListNodeAttr(node string, baseUrl string, token interface{}, attr string) (string, int, string) {
+	var endpoint string
+	var timeout time.Duration
 	switch attr {
 	case "status":
-		timeout := 5
-		endpoint := "/_status"
+		timeout = 5
+		endpoint = "/_status"
 	case "inventory":
-		timeout := 15
-                endpoint := "/_inventory"
+		timeout = 15
+                endpoint = "/_inventory"
+	case "detail":
+		timeout = 15
+                endpoint = "/_detail"
 	default:
-		return "", errors.New("Invalid node attr: " + attr)
+		
+		return "", 1, "Invalid node attr: " + attr
 	}
-	url :=  baseurl + "/system/nodes/" + node + endpoint
+	url :=  baseUrl + "/system/nodes/" + node + endpoint
 	httpClient := http.Client{Timeout: time.Second * timeout}
         client := &HttpClient{Client: &httpClient, Headers: http.Header{}}
-        return client.Get(baseUrl, nil, nil)
+        ret, errcode, errmsg := FormatResponse(client.Get(url, nil, token, nil, true))
+        return ret.(string), errcode, errmsg
 }
 
-func ProvisionNode(node string, baseUrl string, token string, osimage string) (string, error) {
-	url := baseurl + "/system/nodes/" + node + "/_operation"
+func ApplyNodes(baseUrl string, token interface{}, nodeattrs interface{}) (string, int, string) {
+	url := baseUrl + "/manager/resmgr"
+	httpClient := http.Client{Timeout: time.Second * 15}
+        client := &HttpClient{Client: &httpClient, Headers: http.Header{}}
+	tmp_data := nodeattrs
+	data := make(map[string]interface{})
+	data["criteria_spec"] = tmp_data
+	data["capacity"] = 1
+	ret, errcode, errmsg := FormatResponse(client.Post(url, nil, token, data, false))
+	if errcode == 0 {
+		for _, value := range ret.(map[string]interface {}) {
+        	        return value.(string), errcode, errmsg
+	        }
+	}
+	return "", errcode, errmsg
+}
+
+func ListFreeNodes(baseUrl string, token interface{}) (string, int, string) {
+	url := baseUrl + "/manager/resmgr"
+	httpClient := http.Client{Timeout: time.Second * 15}
+        client := &HttpClient{Client: &httpClient, Headers: http.Header{}}
+	ret, errcode, errmsg := FormatResponse(client.Get(url, nil, token, nil, true))
+        return ret.(string), errcode, errmsg
+}
+
+func ReleaseNode(node string, baseUrl string, token interface{}) (string, int, string) {
+	url := baseUrl + "/manager/resmgr"
+	httpClient := http.Client{Timeout: time.Second * 10}
+	client := &HttpClient{Client: &httpClient, Headers: http.Header{}}
+	data := make(map[string]string)
+	data["name"] = node
+	ret, errcode, errmsg := FormatResponse(client.Delete(url, nil, token, data, true))
+	return ret.(string), errcode, errmsg
+}
+
+func ProvisionNode(node string, baseUrl string, token interface{}, osimage string) (string, int, string) {
+	url := baseUrl + "/system/nodes/" + node + "/_operation"
 	httpClient := http.Client{Timeout: time.Second * 30}
         client := &HttpClient{Client: &httpClient, Headers: http.Header{}}
 	tmp_data := make(map[string]string)
@@ -44,6 +104,7 @@ func ProvisionNode(node string, baseUrl string, token string, osimage string) (s
 	data := make(map[string]interface{})
 	data["action_spec"] = tmp_data
 	data["action"] = "rinstall"
-	return client.Post(url, nil, data)
+	ret, errcode, errmsg := FormatResponse(client.Post(url, nil, token, data, true))
+	return ret.(string), errcode, errmsg
 }
 
--- xcat/httpclient.go
+++ xcat/httpclient.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
 type HttpClient struct {
@@ -15,11 +16,12 @@ type HttpClient struct {
 	Headers http.Header
 }
 
-func (s *HttpClient) request(method, url string, headers *http.Header, body io.Reader) (req *http.Request, err error) {
+func (s *HttpClient) request(method, url string, headers *http.Header, token interface{}, body io.Reader) (req *http.Request, err error) {
 	req, err = http.NewRequest(method, url, body)
 	if err != nil {
 		return nil, err
 	}
+
 	if headers == nil && body != nil {
 		headers = &http.Header{}
 		headers.Add("Content-Type", "application/json")
@@ -28,7 +30,7 @@ func (s *HttpClient) request(method, url string, headers *http.Header, body io.R
 	return
 }
 
-func (s *HttpClient) Request(method string, url string, params *url.Values, headers *http.Header, body *[]byte) (data interface{}, err error) {
+func (s *HttpClient) Request(method string, url string, params *url.Values, headers *http.Header, token interface{}, body *[]byte, retRaw bool) (data interface{}, err error) {
 	// add params to url here
 	if params != nil {
 		url = url + "?" + params.Encode()
@@ -39,12 +41,15 @@ func (s *HttpClient) Request(method string, url string, params *url.Values, head
 	if body != nil {
 		buf = bytes.NewReader(*body)
 	}
-	req, err := s.request(method, url, headers, buf)
+	req, err := s.request(method, url, headers, token, buf)
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Set("Accept", "application/json")
+	if token != nil {
+		req.Header.Add("Authorization", "token " + token.(string))
+	}
 	var resp *http.Response
 	resp, err = s.Do(req)
 	if err != nil {
@@ -54,12 +59,12 @@ func (s *HttpClient) Request(method string, url string, params *url.Values, head
 	if err != nil {
 		rbody, _ := ioutil.ReadAll(resp.Body)
 		if rbody != nil {
-			data := make(map[string]interface{})
-			errjson := json.Unmarshal([]byte(rbody), &data)
+			val := make(map[string]interface{})
+			errjson := json.Unmarshal([]byte(rbody), &val)
                         if errjson != nil {
                             return "No message from response", err
                         }
-			return data["message"], err
+			return val["message"], err
 		}
 		return nil, err
 	}
@@ -67,7 +72,13 @@ func (s *HttpClient) Request(method string, url string, params *url.Values, head
 	if err != nil {
 		return nil, errors.New("Can not read the message form response")
 	}
-	return string(rbody), nil
+	if retRaw == true {
+		return string(rbody), nil
+	}
+	if err = json.Unmarshal(rbody, &data); err != nil {
+		return nil, err
+	}
+	return data, nil
 }
 
 func (s *HttpClient) Do(req *http.Request) (*http.Response, error) {
@@ -81,51 +92,51 @@ func (s *HttpClient) Do(req *http.Request) (*http.Response, error) {
 	return resp, nil
 }
 
-func (s *HttpClient) Get(url string, params *url.Values, body interface{}) (interface{}, error) {
+func (s *HttpClient) Get(url string, params *url.Values, token interface{}, body interface{}, retRaw bool) (interface{}, error) {
 	var resp interface{}
 	var err error
 	if body != nil {
 		bodyJson, _ := json.Marshal(body)
-		resp, err = s.Request("GET", url, params, nil, &bodyJson)
+		resp, err = s.Request("GET", url, params, nil, token, &bodyJson, retRaw)
 	} else {
-		resp, err = s.Request("GET", url, params, nil, nil)
+		resp, err = s.Request("GET", url, params, nil, token, nil, retRaw)
 	}
 	return resp, err
 }
 
-func (s *HttpClient) Post(url string, params *url.Values, body interface{}) (interface{}, error) {
+func (s *HttpClient) Post(url string, params *url.Values, token interface{}, body interface{}, retRaw bool) (interface{}, error) {
 	bodyJson, err := json.Marshal(body)
-	resp, err := s.Request("POST", url, params, nil, &bodyJson)
+	resp, err := s.Request("POST", url, params, nil, token, &bodyJson, retRaw)
 	return resp, err
 }
 
-func (s *HttpClient) Put(url string, params *url.Values, body interface{}) (interface{}, error) {
+func (s *HttpClient) Put(url string, params *url.Values, token interface{}, body interface{}, retRaw bool) (interface{}, error) {
 	var resp interface{}
 	var err error
 	if body != nil {
 		bodyJson, _ := json.Marshal(body)
-		resp, err = s.Request("PUT", url, params, nil, &bodyJson)
+		resp, err = s.Request("PUT", url, params, nil, token, &bodyJson, retRaw)
 	} else {
-		resp, err = s.Request("PUT", url, params, nil, nil)
+		resp, err = s.Request("PUT", url, params, nil, token, nil, retRaw)
 	}
 	return resp, err
 }
 
-func (s *HttpClient) Delete(url string, params *url.Values, body interface{}) (interface{}, error) {
+func (s *HttpClient) Delete(url string, params *url.Values, token interface{}, body interface{}, retRaw bool) (interface{}, error) {
 	var resp interface{}
 	var err error
 	if body != nil {
 		bodyJson, _ := json.Marshal(body)
-		resp, err = s.Request("DELETE", url, params, nil, &bodyJson)
+		resp, err = s.Request("DELETE", url, params, nil, token, &bodyJson, retRaw)
 	} else {
-		resp, err = s.Request("DELETE", url, params, nil, nil)
+		resp, err = s.Request("DELETE", url, params, nil, token, nil, retRaw)
 	}
 	return resp, err
 }
 
-func (s *HttpClient) Patch(url string, params *url.Values, body interface{}) (interface{}, error) {
+func (s *HttpClient) Patch(url string, params *url.Values, token interface{}, body interface{}, retRaw bool) (interface{}, error) {
 	bodyJson, err := json.Marshal(body)
-	resp, err := s.Request("PATCH", url, params, nil, &bodyJson)
+	resp, err := s.Request("PATCH", url, params, nil, token, &bodyJson, retRaw)
 	return resp, err
 }
 
@@ -133,32 +144,7 @@ func CheckHTTPResponseStatusCode(resp *http.Response) error {
 	switch resp.StatusCode {
 	case 200, 201, 202, 204, 206:
 		return nil
-	case 400:
-		return errors.New("400 bad request")
-	case 401:
-		return errors.New("401 unauthorised")
-	case 403:
-		return errors.New("403 forbidden")
-	case 404:
-		return errors.New("404 not found")
-	case 405:
-		return errors.New("405 method not allowed")
-	case 409:
-		return errors.New("409 conflict")
-	case 413:
-		return errors.New("413 over limit")
-	case 415:
-		return errors.New("415 bad media type")
-	case 422:
-		return errors.New("422 unprocessable")
-	case 429:
-		return errors.New("429 too many request")
-	case 500:
-		return errors.New("500 instance fault / server err")
-	case 501:
-		return errors.New("501 not implemented")
-	case 503:
-		return errors.New("503 service unavailable")
+	default:
+		return errors.New(strconv.Itoa(resp.StatusCode))
 	}
-	return errors.New("Unexpected response status code" + string(resp.StatusCode))
 }
